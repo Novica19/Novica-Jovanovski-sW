3cb88cc05160a3aece44d2cd03ccc8fd8ee16ca7
Novica19 <novicajovanovski1999@gmail.com>
2022-02-18 12:03:49 +0100

    ...

--- src/components/UI/Modal/ModalOverlay.js
+++ src/components/UI/Modal/ModalOverlay.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import styles from './ModalOverlay.module.css';
+import React, { Component } from "react";
+import styles from "./ModalOverlay.module.css";
 
 class ModalOverlay extends Component {
-    render() {
-        return (
-            <div className={`${styles.modal} ${this.props.currency===true ? styles.currency : ''}`}>
-              <div>{this.props.children}</div>
-            </div>
-          );
-    }
+  render() {
+    return (
+      <div
+        className={
+          this.props.currency ? styles["modal__currency"] : styles["modal"]
+        }
+      >
+        <div>{this.props.children}</div>
+      </div>
+    );
+  }
 }
 
 export default ModalOverlay;
